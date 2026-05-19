16cf252bccce088f6ce6fa17223eb356fb74391c
bianprincipidev-ops <bianprincipi.dev@gmail.com>
2026-05-19 12:20:48 -0300

    chore: separar backend del repo de frontend

--- nexus-backend/contacto/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Mensaje
-
-class MensajeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Mensaje
-        fields = '__all__'
--- nexus-backend/contacto/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import enviar_mensaje
-
-urlpatterns = [
-    path('enviar/', enviar_mensaje, name='enviar_mensaje'),
-]
