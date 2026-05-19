85bbc4ac029bcf1a65663d978d2005c5980476bc
bianprincipidev-ops <bianprincipi.dev@gmail.com>
2026-05-19 18:16:33 -0300

    fix: template prefix and trigger new build

--- src/components/hero/contacto/Contacto.tsx
+++ src/components/hero/contacto/Contacto.tsx
@@ -27,7 +27,7 @@ export default function Contacto() {
 
     emailjs.send(
       'service_liffe5n', // Tu Service ID
-      'gwbbqtz', // Tu Template ID
+      'template_gwbbqtz', // Tu Template ID
       templateParams,
       'GrCKGWOalagGhMi92' // Tu Public Key
     )
